1c646c312da36d38dfb6e1d308a4cfa49b840502
Adam Ash <38866767+Verges5@users.noreply.github.com>
2019-01-25 23:58:05 -0800

    added Child values

---
+++ KnightLight/Assets/Scripts/KidManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KidManager : MonoBehaviour
+{
+	//Bravery
+	[SerializeField] int braveryMeterValue = 100;
+	[SerializeField] int braveryDecayRate = 2;
+	float decayTimer = .01f;
+
+	//Speed
+	[SerializeField] float currentSpeed;
+	[SerializeField] float speedWalking = 2f;
+	[SerializeField] float speedRunning = 4f;
+	
+	//Two-Player Flash
+	[SerializeField] float flashAOERadius = 5f;
+	[SerializeField] bool flashOn = true;
+	[SerializeField] float flashTimer = 10f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		currentSpeed = speedWalking;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		setCurrentSpeed();
+		braveryDecay();
+		flashRecharger();
+	}
+
+	private void setCurrentSpeed()
+	{
+		if (monsterInKnightAOE())
+		{
+			currentSpeed = speedRunning;
+		}
+		else
+		{
+			currentSpeed = speedWalking;
+		}
+	}
+
+	private void braveryDecay()
+	{ 
+		decayTimer -= Time.deltaTime;
+		if(decayTimer < 0)
+		{
+			braveryMeterValue -= braveryDecayRate;
+			decayTimer = .01f;
+		}
+	}
+
+	public void flashRecharger()
+	{
+		flashTimer -= Time.deltaTime;
+		if(flashTimer < 0)
+		{
+			flashOn = true;
+			flashTimer = 10f;
+		}
+	}
+}

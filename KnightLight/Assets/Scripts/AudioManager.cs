1de426d31bf9b0c3d3d2d64227a94feee22d9fbf
UwU <stcastro13@gmail.com>
2019-01-27 10:30:33 -0800

    audiomanager

---
+++ KnightLight/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+    public Sound[] sounds;
+
+	// Use this for initialization
+	void Awake () {
+		foreach(Sound s in sounds)
+        {
+           s.source =  gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+	}
+
+    private void Start()
+    {
+        Play("Theme");
+    }
+    public void Play (string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.source.Play();
+    }
+}

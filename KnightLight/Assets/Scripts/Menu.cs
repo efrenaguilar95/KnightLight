3065ac6fbb8424ced1b1ad03bd50a7110dcaa6e8
Adam Ash <38866767+Verges5@users.noreply.github.com>
2019-01-27 11:30:45 -0800

    Added Menu

---
+++ KnightLight/Assets/Scripts/Menu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void PlayGame()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+
+	public void SeeCredits()
+	{
+		SceneManager.LoadScene("Credits");
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
+}

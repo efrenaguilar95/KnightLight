bb5767549f30939185220c88cc7ea4595c6fd4a1
Ricky Tham <rickytham123@gmail.com>
2019-01-27 12:53:04 -0800

    Camera movement to go Next level
    Camera movement goes to next level when key is found.

---
+++ KnightLight/Assets/Scripts/DoorToNextLevel.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorToNextLevel : MonoBehaviour
+{
+
+    public GameObject kidGameObject;
+    public Camera cam;
+    //public bool isPaused;
+    private bool showDoor;
+    public float timeBetweenPause = 1f;
+    public float countdown = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        kidGameObject = GameObject.FindGameObjectWithTag("Kid");
+        cam = Camera.FindObjectOfType<Camera>();
+        if (kidGameObject == null)
+        {
+            Debug.LogError("No Kid referrenced.");
+        }
+
+
+        showDoor = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Updating");
+        if (startTimer())
+        {
+            countdown -= Time.deltaTime;
+            if (showDoor)
+            {
+                this.gameObject.transform.GetChild(2).GetComponent<Light>().enabled = true;
+            }
+        }
+        else
+        {
+            changeCamera();
+        }
+    }
+
+    private bool startTimer()
+    {
+        if (countdown >= 0) //if countdown is set
+        {
+            return true;
+        }
+        return false; 
+    }
+
+    private void changeCamera()
+    {
+        if (!showDoor)
+        {
+            Debug.Log("ShowDoor = false");
+            if (kidGameObject.GetComponent<KidManager>().hasKey)
+            {
+                Debug.Log("Kid Has Key");
+                //Put Audio of unlocking door here
+                cam.GetComponent<SingleTargetCamera>().enabled = true;
+                cam.GetComponent<MultipleTargetCamera>().enabled = false;
+                countdown = timeBetweenPause;
+                showDoor = true;
+            }
+        }
+        else
+        {
+            cam.GetComponent<MultipleTargetCamera>().enabled = true;
+            cam.GetComponent<SingleTargetCamera>().enabled = false;
+        }
+    }
+
+    public bool showedDoor()
+    {
+        return showDoor;
+    }
+}
---
+++ KnightLight/Assets/Scripts/SingleTargetCamera.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class SingleTargetCamera : MonoBehaviour
+{
+
+    public Transform target;
+    public Vector3 offset;
+    public float smoothTime = .5f;
+    public float minZoom = 40f;
+    public float maxZoom = 10f;
+    public float zoomLimiter = 50f;
+
+
+    private Vector3 velocity;
+    private Camera cam;
+
+    private void Start()
+    {
+        cam = GetComponent<Camera>();
+
+    }
+
+    void Update()
+    {
+        if(target == null)
+        {
+            return;
+        }
+
+        Move();
+    }
+
+    void Zoom()
+    {
+        Debug.Log(GetGreatestDistance());
+        float newZoom = Mathf.Lerp(maxZoom, minZoom, GetGreatestDistance() / zoomLimiter);
+        cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, newZoom, Time.deltaTime);
+
+    }
+
+    void Move()
+    {
+        Vector3 centerPoint = GetCenterPoint();
+
+        Vector3 newPosition = centerPoint + offset;
+
+        transform.position = Vector3.SmoothDamp(transform.position, newPosition, ref velocity, smoothTime);
+
+    }
+
+    float GetGreatestDistance()
+    {
+        var bounds = new Bounds(target.position, Vector3.zero);
+        //for(int i = 0; i< target.Count; i++)
+        //{
+        //    bounds.Encapsulate(targets[i].position);
+        //}
+        return bounds.size.x;
+    }
+
+    Vector3 GetCenterPoint()
+    {
+        //if(targets.Count == 1)
+        //{
+        //    return targets[0].position;
+        //}
+
+        var bounds = new Bounds(target.position, Vector3.zero);
+        //for(int i = 0; i < targets.Count; i++)
+        //{
+        bounds.Encapsulate(target.position);
+        //}
+        return bounds.center;
+
+    }
+
+
+}
